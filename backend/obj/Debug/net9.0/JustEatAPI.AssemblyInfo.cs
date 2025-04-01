141975b41b1e4586caf19a0376741f5625081dbe
deryosssss <w2115152@westminster.ac.uk>
2025-04-01 01:32:05 +0100

    mobile layout

--- backend/obj/Debug/net9.0/JustEatAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/JustEatAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JustEatAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f358ab400b14193f68188b85df24dce2638c8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8428225f2bfaa0e5aeb5a1d29ebbb7f252507c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JustEatAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JustEatAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
